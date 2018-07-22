1479c8f2838e432daa339a2046cd5eb01adc3193
AvasilcaiAlex <matrixtmofficial@yahoo.com>
2018-07-22 19:07:20 +0300

    Add files via upload

---
+++ EOTO.COM/app_code/GlobalVariables.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// GlobalVariables.cs
+/// Clasa speciala in care stocam variabele globale pentru gestionarea lor in pagina
+public static class GlobalVariables
+{
+    public static string ConnectionString = @"Data Source=.\SQLEXPRESS;AttachDbFilename=|DataDirectory|\database.mdf;Integrated Security=True;User Instance=True";
+
+    public static string[] keywords;
+}
