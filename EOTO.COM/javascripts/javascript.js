188a784ad63f7ece5d728c907bf92b520198b341
AvasilcaiAlex <matrixtmofficial@yahoo.com>
2018-07-22 19:47:49 +0300

    Add files via upload

---
+++ EOTO.COM/javascripts/javascript.js
@@ -0,0 +1,17 @@
+﻿$('#navProfileDesktop')
+    .dropdown({
+        action: 'hide'
+    })
+
+$('#modal')
+    .modal('attach events', '#show_modal', 'show')
+    ;
+
+function show_hide() {
+    var x = document.getElementById("toggle");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
